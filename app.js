1a49446ba700ab6e7e1cadd579f6c42d58472574
Breno Carvalho <carv.breno.dev@gmail.com>
2024-09-07 21:51:07 -0300

    refactor: adding first express lines and listen method substitution

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const http = require("http");
+// const http = require("http");
 
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+// Função que irá executar em toda request recebida
+// next é uma função que será passada posteriomente (próximo middleware)
+app.use((req, res, nextMid) => {
+  console.log("in the middleware");
+  nextMid();
+});
 
-server.listen(3001);
+app.use((req, res, nextMid) => {
+  console.log("in the other middleware");
+  // enviando respostas você acaba a chain de middlewares
+  // express response treatment
+  res.send("<h1>Hello from express</h1>");
+});
+
+app.listen(3001);
+// Ambas as funções a seguir são substituidas pelo método .listen(port)
+// do Express
+// const server = http.createServer(app);
+// server.listen(3001);
