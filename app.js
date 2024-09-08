d6fff592cf0c03610ff84b09ae065987096b4548
Breno Carvalho <carv.breno.dev@gmail.com>
2024-09-08 17:36:10 -0300

    feat: add new middleware types and specific method handlers + add body-parser external pkg

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require("express");
 
+//add body-parser dep.
+const bodyParser = require("body-parser");
+
 const app = express();
 
+// registra um middleware para parsear o body request
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, nextMid) => {
-  // Sem chamar o nextMiddleware Fn, eu nunca executo o próximo middleware
-  // exceto se caso meu path seja de fato diferente de /add-product
-  console.log("Estou na página de adicionar produto");
-  res.send("<h1>Bem vindo à página de adição de produtos</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' />\
+    <button type='submit'>Add new product</button></form>"
+  );
+});
+
+// forma de filtrar baseando-se no tipo da requisição
+// esse middleware só disparará em requests do tipo POST
+app.post("/product", (req, res, nextMid) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res) => {
-  // o console irá disparar independentemente da ausência de um next()
-  // no middleware anterior, por questão de como a fn .use funciona
-  // capturando todos os comportamentos de rota
-  console.log("Estou na página padrão");
-
   res.send("<h1>Olá, bem vindo à minha página padrão.</h1>");
 });
 
