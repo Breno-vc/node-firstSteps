d84f91994aa9b2be8ec41e5898411073d7abc073
Breno Carvalho <carv.breno.dev@gmail.com>
2024-09-12 17:42:10 -0300

    final: last commit for this module

--- scripts/shop.js
+++ scripts/shop.js
@@ -2,7 +2,6 @@ const ulProducts = document.getElementById("ul-products");
 
 export default function updateProductsList() {
   ulProducts.innerHTML = "";
-  console.log("oi");
   const array = JSON.stringify(sessionStorage.products);
   const li = document.createElement("li");
   li.textContent = array;
