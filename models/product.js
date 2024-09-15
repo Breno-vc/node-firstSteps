4a70087453fd0ee23169cf08bf8b0f7d28174b7f
Breno Carvalho <carv.breno.dev@gmail.com>
2024-09-15 14:31:42 -0300

    last module commit

--- models/product.js
+++ models/product.js
@@ -1,21 +1,23 @@
 const fs = require("fs");
 const projectRoot = require("../utils/path");
 const path = require("path");
-
 const p = path.join(projectRoot, "data", "products.json");
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return callback([]);
+    }
+    callback(JSON.parse(fileContent));
+  });
+};
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    fs.readFile(p, (error, fileContent) => {
-      let products = [];
-      if (!error) {
-        products = JSON.parse(fileContent);
-      }
-      // importante, se uma função declarada fosse usada (em vez da arrow function como argumento do método da classe) THIS perderia seu contexto
-      // e passaria a se referir ao objeto intero à nova função, em vez de se referir ao objeto da classe PRODUCT
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -26,14 +28,8 @@ module.exports = class Product {
   // static keyword é usada para garantir que o acesso ao método seja feito com a iniciação da classe diretamente, sem a necessidade de criar primeiro um exemplo
   // para só então acessar o método, exemplo em método estático:
   // const planes = Planes.getAllPlaneModels(); (GetAllPlaneModels é método estático de Planes) >> método não estático: const planes = new Planes(''); const allPlanes = planes.getAllPlanes();
-  static async fetchAll(callback) {
+  static fetchAll(cb) {
     // return products;
-    await fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        callback([]);
-      }
-
-      callback(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
