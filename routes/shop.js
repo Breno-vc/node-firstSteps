9572d75caa0f35e6147c0fcc00008e6788cf46a1
Breno Carvalho <carv.breno.dev@gmail.com>
2024-09-23 10:57:47 -0300

    feat: add dynamic routing and advanced model for each product

--- routes/shop.js
+++ routes/shop.js
@@ -8,6 +8,10 @@ const shopController = require("../controllers/shop");
 router.get("/", productsController.getProducts);
 
 router.get("/cart", shopController.getCart);
+
 router.get("/checkout", shopController.getCheckoutPage);
 
+// rotas dinâmicas devem permanecer em baixo do código porquê o express faz uma leitura top-to-bottom, ou seja, todas as rotas posteriores à /products/* seriam ignoradas e tratadas como
+// /:productId
+router.get("/products/:productId", productsController.getProduct);
 module.exports = router;
